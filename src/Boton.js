a54c2deae916afb65afe892776f83ef29b3a3cb2
Edwin Calderon <edwincalderon07@outlook.com>
2020-02-14 19:32:52 -0500

    boton agregado

--- src/Boton.js
+++ src/Boton.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
 import buttomImg from './add.png';
 import './Boton.css';
 
-const Button = () => (
-    <Link to="/exercise/new">
-        <img src={buttomImg} className="Fitness-Add" alt="exercise"/>
-    </Link>
-)
+function Boton() {
+    return (
+        <BrowserRouter>
+            <Link to="/exercise/new">
+                <img src={buttomImg} className="Fitness-Add" alt="exercise" />
+            </Link>
+        </BrowserRouter>
+    )
+}
+
+
 
 export default Boton
